6c72ad515356d3be388ef3c9cc248d455aab00e3
Abel Lavieri <abel.lavieri@gmail.com>
2025-04-26 17:45:24 -0400

    fix paypal integration

--- src/app/Components/utils/paypalcheckout.jsx
+++ src/app/Components/utils/paypalcheckout.jsx
@@ -1,17 +1,19 @@
 import React, { useState } from 'react';
 import { PayPalButtons, usePayPalScriptReducer } from "@paypal/react-paypal-js";
+import { getCookie } from 'cookies-next/client';
 
 const Checkout = () => {
     const [{ options, isPending }, dispatch] = usePayPalScriptReducer();
-    const [currency, setCurrency] = useState(options.currency);
+    const [amount, setAmount] = useState("0.00");
+
+    const handleChange = (e) => setAmount(e.target.value);
 
     const onCreateOrder = (data,actions) => {
-
         return actions.order.create({
             purchase_units: [
                 {
                     amount: {
-                        value: "8.99",
+                        value: amount,
                     },
                 },
             ],
@@ -19,22 +21,34 @@ const Checkout = () => {
     }
 
     const onApproveOrder = (data,actions) => {
-        return actions.order.capture().then((details) => {
-            const name = details.payer.name.given_name;
-            alert(`Transaction completed by ${name}`);
-        });
+        const capturedOrder = fetch("http://localhost:8000/api/v1/solicitudes/capture-order/", {
+            method: "POST",
+            headers: {
+                'Content-Type': 'application/json',
+                'X-CSRFToken': getCookie('csrftoken')
+            },
+            body: JSON.stringify({
+                order_id: data.orderID
+            })
+        })
+        return capturedOrder;
     }
 
     return (
         <div className="checkout">
             {isPending ? <p>LOADING...</p> : (
-                <>
+                <div>
+                    <label>
+                        Enter amount to pay: 
+                        <input value={amount} onChange={handleChange} placeholder="0.00" />
+                    </label>
                     <PayPalButtons 
-                        style={{ layout: "vertical" }}
+                        style={{ layout: "vertical", color: "blue" }}
                         createOrder={(data, actions) => onCreateOrder(data, actions)}
                         onApprove={(data, actions) => onApproveOrder(data, actions)}
+                        fundingSource={"paypal"}
                     />
-                </>
+                </div>
             )}
         </div>
     );
