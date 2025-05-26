2137ada90e7e4f8e1a02b8c8071d42703cf58cc4
EddinsonC <eddinson.c588@gmail.com>
2025-05-26 16:19:35 -0400

    build

--- src/app/(Colegiado)/Colegiado/Solicitudes/page.jsx
+++ src/app/(Colegiado)/Colegiado/Solicitudes/page.jsx
@@ -3,7 +3,6 @@
 import ListaSolicitudes from '@/app/PanelControl/(Solicitudes)/Solicitudes/page'
 
 const ListaSolicitudesColegiado = () => {
-  const {colegiadoId} = useColegiadoUserStore();
   return (
     <>
         <ListaSolicitudes isAdmin={false} />
